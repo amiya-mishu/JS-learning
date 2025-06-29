17a75a47e5e15a389a52ebb063498041d84eaa29
amiya-mishu <amiyamishu5@gmail.com>
2025-06-29 13:27:47 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const account_id=7993
+let account_email="hlo@123"
+var account_password= "3280"
+account_city="Delhi"
+//account_id=4995---->not possible as it is a constant 
+account_email="hii@123"
+let account_state
+console.table([account_email,account_id, account_password, account_state, account_city])
