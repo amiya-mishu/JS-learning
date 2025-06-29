307e93d748597b469236912276c655cd01d4b7eb
amiya-mishu <amiyamishu5@gmail.com>
2025-06-29 19:51:47 +0530

    Update variables.js

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -4,5 +4,6 @@ var account_password= "3280"
 account_city="Delhi"
 //account_id=4995---->not possible as it is a constant 
 account_email="hii@123"
+// dont take var because of the issue in block scope and functional scope  
 let account_state
 console.table([account_email,account_id, account_password, account_state, account_city])
