dec7e3095d3d8f4c11e443fcf46634d86e9028d8
amiya-mishu <amiyamishu5@gmail.com>
2025-06-30 04:23:50 +0000

    container config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+/*
+String-->to store words 
+Number--->to store nos. ranging from 2^32 types
+Bigint---> long walakaam
+Boolean--->true or false
+Undefined---> jb bhi koi new variable define kiye ho
+Null--->value
+Symbol
+Object--->
+
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+The object (person) in the example above has 4 properties: firstName, lastName, age, and eyeColor.
+
+
+let y = 123e5;    gives  12300000
+let z = 123e-5;    gives 0.00123
+*/
+
+let a="john"
+console.log(a)
+console.log(typeof(a))
