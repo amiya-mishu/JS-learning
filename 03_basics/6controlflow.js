ddd7ff9fbb57dc78bf6fd39de98ba1d01660ddf6
amiya-mishu <amiyamishu5@gmail.com>
2025-07-15 09:24:48 +0000

    half control flow done

---
+++ 03_basics/6controlflow.js
@@ -0,0 +1,39 @@
+//if 
+//special case 
+if(2=="2")
+{
+    console.log("executed")
+}
+
+
+if(2==="2")
+{
+    console.log("ex")
+}
+// !== or === ye dono ko dhyan me rkhna 
+// syntax if-else
+// if(...){
+//}else{
+//
+//}
+
+
+const score=200
+if(score>100)
+{
+    const power= "fly"
+    console.log(`User power:${power}`)
+
+}
+ //console.log(`User power:${power}`)---> ye execute nhi hoga 
+ //wrong practice: if (balane>500) console.log("test"), console.log("test2");
+ 
+
+ /* if(){
+ }else if (){
+ }else{
+    }
+    */
+
+
+    
