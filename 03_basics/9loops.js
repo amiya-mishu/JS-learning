98c412c5d7d94375b76c3ff2cdf8947926e73d24
amiya-mishu <amiyamishu5@gmail.com>
2025-07-15 14:21:49 +0000

    loops done

--- 03_basics/9loops.js
+++ 03_basics/9loops.js
@@ -58,10 +58,127 @@ map.set ('usa', "unitedstates of america")
 
 map.set('IN', "india")// ek hi baar print krega kyuki unique value ko store krta h ye 
 
+
+
 console.log(map)
 for (const [key, value] of map) {
     console.log (key ,"-->",  value)
 }
+
+
+
+
+const myObject={
+    'game1':'NFS', 
+    'game2' : 'Spiderman'
+}
+//for (const [key, value] of myObject) {
+//    console.log(key, value)
+//}
+// ye aise kaam nhi krega
+
+
+
+const language={
+    js:'javascript', 
+    cpp:'cPP', 
+    rb:'ruby', 
+    sw:'swift'
+
+}
+//for in loop
+for (const key in language) {
+   
+    console.log(key)
+
+}
+
+for (const key in language) {
+   
+    console.log(language[key])
+    // also can be written as backticks 
+    console.log(`${key} shortcut for ${language[key]}`)
+}
+
+
+
+
+
+const program =["hii", "hiii", "hiiii", "hiiiii"]
+for (const key in program) {
+   
+    console.log(key)
+}
+
+
+
+// for-in loop cant iterate(means doesn't work on map ) map functions 
+
+
+
+//------forEach loop () ---mostly used
+
+const coding =["js", "java", "ruby", "cpp"]
+coding.forEach(function (item){
+    // ye baari baari se hr item of the array ko ek ek krke leke aaega apne pe for iteration
+    console.log (item)
+})
+
+// arrow operator bhi use kr skte h
+
+coding.forEach((item) =>  {
+    console.log(item)
+    
+});
+
+
+
+// considering a function by own and then analyzing it 
+function printMe ( item){
+    console.log(item)
+
+}
+coding.forEach(printMe)
+
+
+// basicaaly a lot more can be contained in for each loop 
+// item, index , and also the arr 
+// therfore each element has the access to the whole array
+
+coding.forEach( (item, index, arr)=>{
+    console.log(item, index, arr)
+}
+)
+
+// ab object k saath kr rhe 
+const myCoding =[
+    {
+        languageName: "JAVA", 
+        languageFileName: "java"
+
+    }, 
+    {
+        languageName: "c++", 
+        languageFileName: "cpp"
+    }, 
+    {
+        languageName: "python", 
+        languageFileName: "py"
+
+    }
+]
+myCoding.forEach((item)=>{
+
+    console.log(item.languageName)
+}
+)
+
+
+
+
+
+
+
 
 
 
