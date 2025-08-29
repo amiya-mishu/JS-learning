aa208128828c36733bad920f61bab3f84d50d34d
amiya-mishu <amiyamishu5@gmail.com>
2025-08-29 20:23:04 +0000

    DOM done

---
+++ 04_part2/1_windows.js
@@ -0,0 +1,39 @@
+/*
+DOM -full conccept:-
+create a html file
+title -do learning
+div-h1, <p>, 
+DOM ka diagram vagarah
+inspect a website
+go to console
+window me jao .bhut kuchhh aata h 
+(windows.)-->ye krke kafi cheeze access kr skte h 
+
+windows.dir - krne se bhut se aur bhi commands ya phir cheezon ki acces milti hai 
+
+
+diagram bna rhe smjhne ko :-
+windows
+   |
+document
+   |
+  html
+(divided into 2 parts)
+1.head,                              2. body
+  |      \                               |
+title   meta                         div
+|         \                            |
+attributes attributes               attribute  
+                                       / \
+                                      h1 <p> 
+
+
+console.log(document.(...))=>(...)can be many functions hidden or unnecessary
+
+
+till 18th minutes
+*/
+
+
+
+
